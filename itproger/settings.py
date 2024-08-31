1db311621d7b5e12c2ab74352b9c80b624e7fa00
8907 <kerimkul_diar99@mail.ru>
2024-08-31 21:25:20 +0500

    132

--- itproger/settings.py
+++ itproger/settings.py
@@ -136,4 +136,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'main\static'), )
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'main/static'),
